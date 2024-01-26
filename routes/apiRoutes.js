d9cf260a5f13d61a2ed6617fd01caad9ba96ca2e
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-26 18:22:41 -0500

    added the receiveFile so it could receive the newNotes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const fs = require('fs')
+const fs = require('fs');
+const { title } = require('process');
 router.get('/notes',(req, res)=>{
 fs.readFile("./db/db.json","utf-8",(err,data)=>{
     if (err) {
@@ -12,18 +13,21 @@ fs.readFile("./db/db.json","utf-8",(err,data)=>{
     res.json(notes)
 })
 })
-// router.get('/notes',(req, res)=>{
-// fs.readFile("./db/db.json","utf-8",(err,data)=>{
-//     if (err) {
-//         console.log(err);
+
+
+router.post('/notes',(req, res)=>{
+fs.receiveFile(newNote, "./db/db.json","utf-8",(success,newNote)=>{
+    // const {title,text} = req.body
+    if (title && text) { newNotes=title,text, 
+        console.log(newNote);
         
-//     }
-//     console.log(data);
-//     let notes = JSON.parse(data)
-//     console.log(notes);
-//     res.json(notes)
-// })
-// })
+    }
+    console.log(success);
+    let newNotes = JSON.parse(success)
+    console.log(newNotes);
+    res.json(newNotes)
+})
+})
 // router.get('/notes',(req, res)=>{
 // fs.readFile("./db/db.json","utf-8",(err,data)=>{
 //     if (err) {
