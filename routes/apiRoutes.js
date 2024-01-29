a9aaf588f90d5645688eec90f20fc6b9b75dd74d
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-29 16:08:10 -0500

    added a well generated readme for the challange and deleted out comments on the page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,38 +18,10 @@ router.post("/notes", (req, res) => {
     text: req.body.text,
   };
   // get the current notes and add the new one to them
-  readAndAppend(newNote, "./db/db.json");
+  readAndAppend(newNote, "./db/db.json")
+   res.json('works!');
 });
 
-// router.delete('/notes/id',(req, res)=>{
-//   database
-//   .deleteNotes()
-//   .then(notes) => {
-//     return res.json
-//   }
-// // fs.deleteFile("./db/db.json","utf-8",(err,data)=>{
-//     if (notes) { notes = find, id
-//         console.log();
-
-//     }
-//     console.log();
-//     let notes = JSON.parse()
-//     console.log();
-//     res.json()
-// })
-// })
 
 module.exports = router;
 
-// router.post("/notes", (req, res) => {
-//   fs.receiveFile(newNote, "./db/db.json", "utf-8", (err, data) => {
-//     // const {title,text} = req.body
-//     if (title && text) {
-//       (newNotes = title), text, console.log(err);
-//     }
-//     console.log(data);
-//     let newNotes = JSON.parse(success);
-//     console.log(newNotes);
-//     res.json(newNotes);
-//   });
-// })
