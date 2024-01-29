ee8b7baac0b8a712936f5164c811a0869496168d
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-29 15:28:55 -0500

    helpers and routers update notes are showing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,25 @@
 const router = require("express").Router();
 const fs = require("fs");
 const { title } = require("process");
-const database = require("../db/db.json");
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
 const { response } = require("express");
+
+
 router.get("/notes", (req, res) => {
-  database
-    .getNotes()
-    .then((notes) => {
-      return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-router.post("./notes", (req, res) => {
-  console.log(req.body)//data
-    const newNote = {
-      title:req.body.title,
-      text:req.body.text}
- database.saveNotes()
-  .then((notes) => {
-    return res.json(response );
-  })
-  .catch((success) => res.status(500).json(success))
+router.post("/notes", (req, res) => {
+  console.log(req.body); //data
+  //create the new note from the request pay load
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+  };
+  // get the current notes and add the new one to them
+  readAndAppend(newNote, "./db/db.json");
 });
-
 
 // router.delete('/notes/id',(req, res)=>{
 //   database
--- server.js
+++ server.js
@@ -16,15 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(routes);
-// // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
 
-// // GET Route for feedback page
-// app.get('/feedback', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
